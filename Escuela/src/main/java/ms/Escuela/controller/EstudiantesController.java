fde455161d2a5356e8b4cdce3cf47c048c5c04dc
Alex <bado_agp@hotmail>
2023-06-19 22:57:11 -0600

    info nueva

--- Escuela/src/main/java/ms/Escuela/controller/EstudiantesController.java
+++ Escuela/src/main/java/ms/Escuela/controller/EstudiantesController.java
@@ -1,4 +1,21 @@
 package ms.Escuela.controller;
 
+import ms.Escuela.entity.Estudiantes;
+import ms.Escuela.service.EstudiantesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/estudiantes")
 public class EstudiantesController {
+    @Autowired
+    EstudiantesService estudiantesService;
+
+    @PostMapping("/guardarEstudiantes")
+    public Estudiantes guardarEstudiantes(@RequestBody Estudiantes estudiantes){
+        return estudiantesService.guardarEstudiante(estudiantes);
+    }
 }
